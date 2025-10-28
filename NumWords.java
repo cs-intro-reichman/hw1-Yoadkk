91cea9935a68d5c0d4aa7f436607a84ba4dc3718
YoadKlein <yoadklein@icloud.com>
2025-10-28 23:19:32 +0200

    I was so close last time but now i got this

--- NumWords.java
+++ NumWords.java
@@ -1,21 +1,16 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-		
+
 		// Declare num - the integer that we will divide to hundreds, tens and ones.
 		// The input will come from the terminal
 		int num = Integer.parseInt(args[0]);
-		// I make versions of the numbers where the hundred||ten , and one values are 0 
-		int zerosAfterThousand = (num/1000)*1000; // if num = 12331322 -> zerosAfterThousand = 1231000
-		int zerosAfterHundreds = (num/100)*100; // if num = 12331322 -> zerosAfterHundred = 1231300
 
-		// I take my zerosAfter values and subtract them from the original number in order to get a 3 or 2 digit number
+		int hundreds = num/100; // This just gives me the number of the hundreds without a limit - if a number is 5000 it will give you the number of all the hundreds - in this case - 50
+		int tens = (num-(hundreds*100))/10; //Now there must be a 2 digit number and when i devide by 10 i get the numbers of tens
+		int ones = (num-(hundreds*100)) - (tens*10); // In this formula i get the last 2 digit and subtract it by the tens to get a single number - and thats my number of ones!
 
-		int hundreds = (num - zerosAfterThousand)/100; // Now there must be a 3 digit number and when i devide by 100 i get the numbers of hundreds
-		int tens = (num - zerosAfterHundreds)/10; // Now there must be a 2 digit number and when i devide by 10 i get the numbers of tens
-		int ones = (num - zerosAfterHundreds) - tens*10; // In this formula i get the last 2 digit and subtract it by the tens to get a single number - and thats my number of ones!
-		
-
+		// Print the message 
 		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 
 
